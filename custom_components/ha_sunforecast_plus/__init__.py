7b34b7b65d33642d72062c47b4141949c616e2c4
chourmovs <chourmovs@gmail.com>
2025-05-17 18:51:45 +0200

    beta1 branch

--- custom_components/ha_sunforecast_plus/__init__.py
+++ custom_components/ha_sunforecast_plus/__init__.py
@@ -7,10 +7,10 @@ from homeassistant.const import Platform
 from homeassistant.core import HomeAssistant
 
 from .const import DOMAIN
-from .exceptions import OpenMeteoSolarForecastUpdateFailed  # noqa: F401
+#from .exceptions import OpenMeteoSolarForecastUpdateFailed  # noqa: F401
 from .coordinator import OpenMeteoSolarForecastDataUpdateCoordinator
 #from .models import Estimate
-from .open_meteo_solar_forecast import OpenMeteoSolarForecast
+#from .open_meteo_solar_forecast import OpenMeteoSolarForecast
 
 __all__ = [
     "Estimate",
